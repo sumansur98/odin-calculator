a9b407cd3d3d488d3649d37957597c4f9614fd61
Suman Sur <sumansur.cs98@gmail.com>
2023-09-20 20:33:32 +0530

    basic operation functions added

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(num1, num2){
+    return num1+num2;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function divide(a,b){
+    return a/b ;
+}
+
