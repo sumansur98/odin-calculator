ebc6918f93ee661f8cee7545d296ea5f5be14d67
Suman Sur <sumansur.cs98@gmail.com>
2023-10-10 21:14:54 +0530

    wrote operate function

--- script.js
+++ script.js
@@ -14,3 +14,9 @@ function divide(a,b){
     return a/b ;
 }
 
+function operate(operator, a, b){
+    if(operator === '+')    add(a,b)
+    if(operator === '-')    subtract(a,b)
+    if(operator === '*')    multiply(a,b)
+    if(operator === '/')    divide(a,b)
+}
