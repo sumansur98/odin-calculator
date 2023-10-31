d7582d2c14c26ffb246c37fd2b9b844bf8a4aaa1
Suman Sur <sumansur.cs98@gmail.com>
2023-10-31 21:25:58 +0530

    basic html layout

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let isChain = false;
+let firstNum, secondNum, result;
 function add(num1, num2){
     return num1+num2;
 }
@@ -20,3 +22,14 @@ function operate(operator, a, b){
     if(operator === '*')    multiply(a,b)
     if(operator === '/')    divide(a,b)
 }
+
+function display(num){
+    let currText = document.querySelector("#calculator-display").textContent;
+    let updateText = `${currText}${num}`;
+    inputNumber = Number(updateText);
+    document.querySelector("#calculator-display").textContent = updateText;
+}
+
+
+
+
